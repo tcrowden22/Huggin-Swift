d017ba7d7a1fda83a33ede5d4cd578b1bbfb73dc
tcrowden22 <64867342+tcrowden22@users.noreply.github.com>
2025-06-09 13:36:26 -0500

    Initial Commit

---
+++ Huggin-MACOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Huggin-MACOS
+//
+//  Created by TJ Crowden on 6/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Huggin-MACOS/Huggin_MACOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Huggin_MACOSApp.swift
+//  Huggin-MACOS
+//
+//  Created by TJ Crowden on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Huggin_MACOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
